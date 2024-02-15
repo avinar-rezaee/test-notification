03d1672ef0f87d7dbd9affe42e7c2dbf86d9c48f
avinar rezaee <avinar.rezaee@gmail.com>
2024-02-15 12:31:23 +0330

    avin

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@ console.log("Service Worker Loaded...");
 self.addEventListener("push", e => {
     const data = e.data.json();
     console.log("Push Recieved...");
+    console.log({ data });
     // document.querySelector('#logs').append("Push Recieved...")
     // document.querySelector('#logs').append(document.createElement('br'))
     self.registration.showNotification(data.title, {
